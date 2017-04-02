bcf92a7ec8621d385ed6be087d001b8001f14a6d
Alex Gubin <trace1245@gmail.com>
2017-04-02 12:42:06 +0300

    first commit

---
+++ SimpleAspNetApp/SimpleAspNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleAspNetApp.Startup))]
+namespace SimpleAspNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

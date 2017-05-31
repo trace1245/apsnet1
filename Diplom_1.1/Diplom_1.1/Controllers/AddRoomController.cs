67b143d65159ed651487765c78afe83a578d87c3
Alex Gubin <trace1245@gmail.com>
2017-05-31 16:58:26 +0300

    v1.1 added AddGroup page

--- Diplom_1.1/Diplom_1.1/Controllers/AddRoomController.cs
+++ Diplom_1.1/Diplom_1.1/Controllers/AddRoomController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Diplom_1._1.ViewModels;
+using Diplom_1._1.Models;
+using Diplom.Models;
 
 namespace Diplom_1._1.Controllers
 {
     public class AddRoomController : Controller
     {
+        MyContext db = new MyContext();
+
         // GET: AddRoom
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            AddRoomViewModel model = new AddRoomViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(AddRoomViewModel model)
+        {
+            db.Groups.Add(new Group { Name = model.NewGroup });
+            db.SaveChanges();
+            model.Added = true;
+            return View(model);
         }
     }
 }
---
+++ Diplom_1.1/Diplom_1.1/ViewModels/AddRoomViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diplom_1._1.ViewModels
+{
+    public class AddRoomViewModel
+    {
+        public string NewGroup { get; set; }
+        public bool? Added { get; set; }
+    }
+}
